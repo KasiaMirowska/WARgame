73919327c3bc698fdc578ae02facea5949e67bb4
Kasia <kasia@miroartdesign.com>
2019-06-13 16:52:12 -0400

    issues within gameDisplay

--- src/scripts/GameDisplay.js
+++ src/scripts/GameDisplay.js
@@ -8,16 +8,28 @@ export default class GameDisplay {
      constructor(){
         this.game = new Game();
      }
+
+    generateStartView() {
+        return (
+            `<section>
+            <h2>PLAYER 1</h2>
+            <h2>PLAYER 2</h2>
+            <h3>Current Stack Amount: hahaha ${this.game.player1.cards.length}</h3>
+            <h3>Current Stack Amount:v blala ${this.game.player2.cards.length}</h3>
+            
+          </section>`
+        )
+    }
     
 
     handleStart() {
-         console.log(this.game.player1) 
-        // let html = (this.generateStartView())
+        console.log(this.game) 
+        let html = (this.generateStartView())
         $('.start').on('click', e => {
             e.preventDefault();
             console.log('am i here?')
              this.game.start();
-            // $('.game-controls').html(html);
+            $('.game-controls').html(html);
         })
 
         
